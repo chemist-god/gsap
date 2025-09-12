1d4c8d9b0b56c06eeb2246374ab65af2c2785209
chemist-god <godtenkorang72@gmail.com>
2025-09-12 10:05:16 +0000

    Update Navbar.tsx: Improved navigation bar component structure and styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,25 +1,27 @@
 
 import React from "react";
 import { navLinks } from "../../constants";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className="fixed z-50 w-full bg-black text-white py-5 px-5 flex flex-col md:flex-row md:justify-between items-center gap-5 container mx-auto">
-      <p className="font-modern-negra text-3xl mb-2">Velvet Pour</p>
-    <ul className="flex flex-center lg:gap-12 gap-7">
-      {navLinks.map(link => (
-        (
-          <li key={link.id}>
-            <a
-              href={`#${link.id}`}
-              className="cursor-pointer text-nowrap md:text-base text-sm"
-            >
-              {link.title}
-            </a>
-          </li>
-        )
-      ))}
-    </ul>
+        <div className="flex items-center gap-2">
+            <Image src="/images/logo.png" alt="logo" width={48} height={48} />
+            <p className="font-modern-negra text-3xl">Velvet Pour</p>
+        </div>
+        <ul className="flex flex-center lg:gap-12 gap-7">
+            {navLinks.map(link => (
+                <li key={link.id}>
+                    <a
+                        href={`#${link.id}`}
+                        className="cursor-pointer text-nowrap md:text-base text-sm"
+                    >
+                        {link.title}
+                    </a>
+                </li>
+            ))}
+        </ul>
     </nav>
   );
 };
